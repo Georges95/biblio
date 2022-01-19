e6619f08a9e762e026db749f7c6ab4d84c5b6a84
georges <georges.laerche@gmail.com>
2022-01-19 16:51:58 +0100

    Ajout de la BDD

--- livres.php
+++ livres.php
@@ -1,17 +1,9 @@
 <?php 
-require_once "Livre.class.php";
-$l1 = new Livre(1,"Algorithmique selon H2PROG",300,"algo.png");
-$l2 = new Livre(2,"Le virus Asiatique",200,"virus.png");
-$l3 = new Livre(3,"La France du 19ème",100,"france.png");
-$l4 = new Livre(4,"Le JavaScript Client",500,"JS.png");
-
 require_once "LivreManager.class.php";
-$livreManager = new LivreManager;
-$livreManager->ajoutLivre($l1);
-$livreManager->ajoutLivre($l2);
-$livreManager->ajoutLivre($l3);
-$livreManager->ajoutLivre($l4);
 
+$livreManager = new LivreManager;
+
+$livreManager->chargementLivres();
 ob_start(); 
 ?>
 
